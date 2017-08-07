7fd7a35aa17a8bdcb5783ad48916f8e1b57ba7c0
Kyle Herrity <kherrity@gmail.com>
2017-08-07 08:04:51 -0500

    Exercise 1.11.

---
+++ ch1/1_11.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter two numbers:" << std::endl;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    if (v1 > v2) 
+    {
+        int t = v1;
+        v1 = v2;
+        v2 = t;
+    }
+    while (v1 <= v2) {
+        std::cout << v1++;
+    }
+    return 0;
+}
